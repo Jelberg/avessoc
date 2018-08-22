45c260c09ee2854306dfd62928b3150b05ac6123
Jessica Elberg <jessica_elberg@hotmail.com>
2018-08-22 16:26:06 -0430

    Todo fluye, mas o menos terminado la plantilla de llenar datos de paciente

--- php/template/template-page-registerpatient.php
+++ php/template/template-page-registerpatient.php
@@ -11,11 +11,7 @@ get_header();
 
 <section class="grid-1">
 
-    <div class="area-1"> <!--PENDIENTE ARREGLAR-->
-        <p>-</p>
-        <p>-</p>
-        <p>-</p>
-    </div>
+
 
     <div class="area-2">
         <h1>Menu</h1>
@@ -24,9 +20,10 @@ get_header();
     <div class="area-3">
         <section class="grid-2">
 
+
+
             <div class="item1">
                 <div class="item-grid-2-border">
-
                     <section class="grid-columns">
                                                     <!-- DATOS PERSONALES -->
                         <div class ="item1">
@@ -113,52 +110,102 @@ get_header();
                 </div>
             </div>
 
+
+
             <div class="item2">
                 <div class="item-grid-2-border">
+                  <section class="grid-columns">
                     <!-- DIRECCION -->
-                
-                    <label for="name">Direccion</label><input type="text" name="direccion" id="direccion" />
-                    <label for="name">Estado</label><select id="estado">
-                    </select>
-                    <label for="name">Municipio</label><select id="municipio">
-                    </select>
-                    <label for="name">Parroquia</label><select id="parroquia">
-                    </select>
+                   <div class = "item1">
+                        <label for="name">Estado</label><br>
+                            <select id="estado" class = "form-area">
+                            </select>
+                        </div>
+
+                        <div class="item2">                  
+                                <label for="name">Municipio</label><br>
+                                <select id="municipio" class="form-area">
+                                </select>
+                        </div>
+
+                        <div class ="item3">
+                                <label for="name">Parroquia</label><br>
+                                <select id="parroquia" class="form-area">
+                                </select>
+                        </div>
+
+                        <div class ="item-span-three">
+                                <label for="name">Direccion</label><br>
+                                <input type="text" class="form-area-three" name="direccion" id="direccion" />        
+                        </div>
                 
                     <!-- FIN DIRECCION -->
+                    </section>
                 </div>
             </div>
+
+
 
             <div class="item3">
                 <div class="item-grid-2-border">
+                    <section class="grid-columns">
                     <!-- CONTACTO -->
-
-                    <label for="name">Telefono Local</label><input type="number" name="local" id="local" />
-                    <label for="name">Telefono Movil</label><input type="number" name="movil" id="movil" />
-                    <label for="name">Correo</label><input type="number" name="correo" id="correo" />
+                        <div class ="item1">
+                            <label for="name">Telefono Local</label><br>
+                            <input type="number" class="form-area-number" name="local" id="local" />
+                        </div>
+                        <div class ="item2">
+                            <label for="name">Telefono Movil</label><br>
+                            <input type="number" class="form-area-number" name="movil" id="movil" />
+                        </div>
+                            
+                        <div class ="item3">
+                            <label for="name">Correo</label><br>
+                            <input type="text"  class="form-area" name="correo" id="correo" />
+                        </div>
 
                     <!-- FIN CONTACTO -->
+                    </section>
                 </div>
             </div>
+
+
 
 
             <div class="item4">
                 <div class="item-grid-2-border">
                     <!-- COMPOSICION FAMILIAR -->
+                    <section class="grid-columns">
+                        <div class = "item1">
+                            <label for="name">Numero de personas que viven en el hogar</label><br>
+                            <input type="number" class="form-area-number" name="num-personas" id="num-personas" />
+                        </div>
+                        <div class = "item2">
+                            <label for="name">Ingreso Promedio Mensual</label><br>
+                            <input type="number" class="form-area-number" name="ingreso-promedio" id="ingreso-promedio" />
+                        </div>
 
-                    <label for="name">Numero de personas que viven en el hogar</label><input type="number" name="num-personas" id="num-personas" />
-                    <label for="name">Ingreso Promedio Mensual</label><input type="number" name="ingreso-promedio" id="ingreso-promedio" />
-                    <label for="name">Tipo de familia con la que convive</label><select id="familia-tipo">
-                        <option value="S">Una sola persona</option>
-                        <option value="C">Pareja sin hijos</option>
-                        <option value="D">Madre/Padre solo, con hijos</option>
-                        <option value="V">Madre y Padre con hijos y otros parientes</option>
-                        <option value="V">Madre/Padre solo, hijos y otros parientes</option>
-                        <option value="V">Niño/a o adolence en entidad de atencion </option>
-                        <option value="V">Otro</option>
-                    </select>
-                    <label for="name">En caso de ser otro especifique</label><input type="text" name="otro-tipo" id="otro-tipo" />
+                        <div class = "item3"></div>
 
+                        <div class = "item-span-two">
+                            <label for="name">Tipo de familia con la que convive</label><br>
+                            <select id="familia-tipo" class="form-area-two">
+                                <option value="S">Una sola persona</option>
+                                <option value="C">Pareja sin hijos</option>
+                                <option value="D">Madre/Padre solo, con hijos</option>
+                                <option value="V">Madre y Padre con hijos y otros parientes</option>
+                                <option value="V">Madre/Padre solo, hijos y otros parientes</option>
+                                <option value="V">Niño/a o adolence en entidad de atencion </option>
+                                <option value="V">Otro</option>
+                            </select>
+                        </div>
+                        
+                        <div class = "item-span-two">
+                            <label for="name">En caso de ser otro especifique</label><br>
+                            <input type="text" class="form-area-two" name="otro-tipo" id="otro-tipo" />
+                        </div>
+
+                    </section>
                     <!-- FIN COMPOSICION FAMILIAR -->
                 </div>
             </div>
@@ -167,22 +214,26 @@ get_header();
             <div class="item5">
                 <div class="item-grid-2-border">
                     <!-- DETALLE LABORAL -->
-
-                    <label for="name">Condicion laboral</label><select id="condicion-laboral">
-                        <option value="S">Desocupado/ Sin trabajo</option>
-                        <option value="C">Oficios del hogar</option>
-                        <option value="D">Trabajo formal (Empresas, Organismos de estados, etc.)</option>
-                        <option value="V">Trabajo informal a destajo, trabajo temporal)</option>
-                    </select>
-
+                    <section class="grid-columns">
+                        <div class = "item-span-two">
+                        <label for="name">Condicion laboral</label><br>
+                        <select id="condicion-laboral" class ="form-area">
+                            <option value="S">Desocupado/ Sin trabajo</option>
+                            <option value="C">Oficios del hogar</option>
+                            <option value="D">Trabajo formal (Empresas, Organismos de estados, etc.)</option>
+                            <option value="V">Trabajo informal a destajo, trabajo temporal)</option>
+                        </select>
+                    </div>
+                    </section>
                     <!-- FIN DETALLE LABORAL -->
                 </div>
             </div>
 
             <div class="item6">
                 <div class="item-grid-2-border">
+                    <ol>
                     <!-- CLASIFICACION GRAFFAR -->
-
+                    <li>
                         <form>
                             <label>Profesion del jefe del hogar</label>
                             <dd><input type="radio" name="UNO" id="uno-1" /><label for="uno-1">Profesion Universitaria</label></dd>
@@ -191,6 +242,8 @@ get_header();
                             <dd><input type="radio" name="UNO" id="uno-4" /><label for="uno-4">Obreros especializados, parte de los trabajadores del sector informal de la economia </label></dd>
                             <dd><input type="radio" name="UNO" id="uno-5" /><label for="uno-5">Obreros no especializados y otra parte del sector informal de la economia</label></dd>
                         </form>
+                    </li>
+                    <li>
 
                         <form>
                             <label>Nivel de instruccion de la esposa o conyugue</label>
@@ -200,7 +253,8 @@ get_header();
                             <dd><input type="radio" name="DOS" id="dos-4" /><label for="dos-4">Enseñanza primaria o alfabeta (Algun grado de instruccion primaria)</label></dd>
                             <dd><input type="radio" name="DOS" id="dos-5" /><label for="dos-5">Analfabeta</label></dd>
                         </form>
-
+                    </li>
+                    <li>
                         <form>
                             <label>Principal fuente de ingreso</label>
                             <dd><input type="radio" name="TRES" id="tres-1" /><label for="tres-1">Fortuna heredada o adquirida</label></dd>
@@ -209,7 +263,8 @@ get_header();
                             <dd><input type="radio" name="TRES" id="tres-4" /><label for="tres-4">Sueldo semanal, por dia. Entrada a destajo</label></dd>
                             <dd><input type="radio" name="TRES" id="tres-5" /><label for="tres-5">Donaciones de origen público o privado</label></dd>
                         </form>
-
+                    </li>
+                    <li>
                         <form>
                             <label>Condiciones de alojamiento</label>
                             <dd><input type="radio" name="CUATRO" id="cuatro-1" /><label for="">Vivienda con optimas condiciones sanitarias y ambientales de gran lujo</label></dd>
@@ -218,9 +273,10 @@ get_header();
                             <dd><input type="radio" name="CUATRO" id="cuatro-4" /><label for="">Viviendas con ambientes espaciosos o reducidos y/o con deficiencias en algunas condiciones sanitarias</label></dd>
                             <dd><input type="radio" name="CUATRO" id="cuatro-5" /><label for="">Rancho o vivienda con espacios insuficientes y condiciones sanitarias marcadamente inadecuadas</label></dd>
                         </form>
-
+                    </li>
 
                     <!-- FIN CLASIFICACION GRAFFAR -->
+                    </ol>
                 </div>
             </div>
 
