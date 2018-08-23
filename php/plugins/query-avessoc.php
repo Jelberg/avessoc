1825aea4250b8f20af4ea5e0ad10bf3473b45891
Jessica Elberg <jessica_elberg@hotmail.com>
2018-08-23 13:33:45 -0430

    plugin para ejecutar los querys

---
+++ php/plugins/query-avessoc.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* Plugin Name: Query's AVESSOC
+* Description: Este plugin esta desarrollado para que pueda ejecutar los query's de la base de datos que le da soporte al portal de avessoc
+* Version: 1.0.0
+* Author: Jessica Elberg
+*/
+?>
+
+<?php
+
+
+?>
