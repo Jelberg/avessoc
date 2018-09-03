1350ae0c85daec01cadb2b7f7bbac6e02c955630
Jessica Elberg <jessica_elberg@hotmail.com>
2018-09-03 08:11:11 -0430

    validation-avessoc

---
+++ php/validation-avessoc/validation-avessoc.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* Plugin Name: Validaciones AVESSOC
+* Description: Plugin desarrollado para agregar las validaciones de los campos de registro de la pagina web
+* Version: 1.0.0
+* Author: Jessica Elberg
+*/
+
+
+
+
+
+?>
