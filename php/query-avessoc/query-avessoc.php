5310d03e83a6d6d06d73852cc19d28f7cd985eaf
Jessica Elberg <jessica_elberg@hotmail.com>
2018-09-06 16:08:37 -0430

    Casi completa la parte de agregar sponsor, falta mensaje de sumit

--- php/query-avessoc/query-avessoc.php
+++ php/query-avessoc/query-avessoc.php
@@ -178,7 +178,15 @@ function add_request($wpdb,$id_paciente,$numpersonas,$ingresopromedio, $familiat
 
 
 //--------------------------------------------CRUD SPONSOR-----------------------------------------------------
-function add_sponsor($nombrelegal,$tipodocumento,$numero,$logo){
+/**
+ *
+ * Inserta el Sponsor a la base de datos
+ * @param $nombrelegal
+ * @param $tipodocumento
+ * @param $numero
+ * @param $logo
+ */
+function add_sponsor($nombrelegal,$tipodocumento,$numero,$logo,$montoinicial){
     global $wpdb;
 
     $wpdb->insert('SPONSOR', array(
@@ -188,9 +196,43 @@ function add_sponsor($nombrelegal,$tipodocumento,$numero,$logo){
         'SPONSOR_LOGO' => $logo
     ));
 
+    if (!empty($montoinicial)){
+        $id_sponsor= $wpdb->get_results( "SELECT MAX(MPERSON_ID) AS id FROM SPONSOR" ); //< Devuelve el ultimo id registrado
+        add_cntribution_init($montoinicial,$id_sponsor);
+    }
+
+}
+
+/**
+ * Buscar el numero de identificacion del sponsor
+ * @param $numIdentificacion
+ * @param $tipo
+ * @return mixed
+ */
+function search_sponsor_id($numIdentificacion, $tipo){
+    global $wpdb;
+    $query ="SELECT MPERSON_ID FROM `SPONSOR` WHERE MPERSON_IDENTF = ".$numIdentificacion." AND MPERSON_TYPE_DOC ='".$tipo."'";
+    $id_sponsor= $wpdb->get_var( $query );
+    return $id_sponsor;
 }
 
 //-------------------------------------------CRUD CNTRIBUTION--------------------------------------------------
+
+/**
+ * Este insert es solo para cuando se agrega por primera vez al sponsor
+ * @param $montoinicial
+ * @param $userid
+ */
+function add_cntribution_init($montoinicial,$userid){
+    global $wpdb;
+
+    $wpdb->insert('CNTBTION', array(
+        'CNTBTION_CANT' => $montoinicial,
+        'CNTBTION_BALANCE' => $montoinicial,
+        'CNTBTION_SPONSOR_ID' => $userid
+    ));
+
+}
 
 ?>
 
--- php/template/template-add-sponsor.php
+++ php/template/template-add-sponsor.php
@@ -11,17 +11,30 @@ get_header();
 
     //Variable para mensaje
     $legalErrmsj="Sólo se permiten mayúsculas, minusculas y espacios en blanco.";
+    $msjNumero="";
 
     //Variable para mantener selecciones
-    $legal=$numedoc=$aporte=$tipodoc="";
+    $legal=$numedoc=$aporte=$tdoc="";
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
         $legal = test_input($_POST['legal-name']);
         $numedoc = test_input($_POST['numero-doc']);
         $aporte = test_input($_POST['aporte']);
-        $tipodoc = test_input($_POST['tipo-documento']);
+        $tdoc = test_input($_POST['tipo-documento']);
 
     }
+
+    if (($_POST["tipo-documento"]) == "") {
+        $TdocErr = "Requerido";
+    } else {
+        $tdoc = test_input($_POST["tipo-documento"]);
+    }
+
+    if ( sizeof(search_sponsor_id($_POST["numero-doc"],$_POST["tipo-documento"])) != 0){
+        $msjNumero ="Identificaion de usuario ya exiaste";
+    }
+
 
     function test_input($data) {
         $data = trim($data);
@@ -63,16 +76,16 @@ get_header();
 
                             <div class="item2" ><!--fila 2-->
                                 <label for="name">Tipo de Documento</label><span class="required">*</span><br>
-                                <select id="tipo-documento" class="form-area-row" required>
-                                    <option value=""> >>Seleccione opción<< </option>
+                                <select id="tipo-documento" name="tipo-documento" class="form-area-row" required>
+                                    <option value="" selected> >>Seleccione opción<< </option>
                                     <?php
 
-                                    $datos = array("RIF","V","E","Passaporte");
-                                    $valor = array("RIF","V","E","P");
+                                    $datos = array("V","E","Passaporte","RIF");
+                                    $valor = array("V","E","Passaporte","RIF");
 
                                     for($i=0; $i<count($datos); $i++)
                                     {
-                                        if($valor[$i]==$tipodoc) // Compara si el valor que se guarda en tdoc se encuentra dentro del arreglo
+                                        if($valor[$i]==$tdoc) // Compara si el valor que se guarda en tdoc se encuentra dentro del arreglo
                                         {
                                             echo "<option value='".$valor[$i]."' selected>".$datos[$i]."</option>";
                                         }
@@ -89,12 +102,14 @@ get_header();
                             <div class="item3" ><!--fila 3-->
                                 <label for="name">Numero del documento</label><span class="required">*</span><br>
                                 <input type="number" name="numero-doc" id="numero-doc" class="form-area-number-row" min="0" value="<?php echo $numedoc?>" required/>
+                                <?php if (!empty($msjNumero)) {  echo "<span class=estiloError>$msjNumero</span>"; }  ?>
                             </div>
 
                             <div class="item4" ><!--fila 4-->
                                 <label for="logo">Logo</label><span class="required">*</span><br>
                                 <p>Sólo archivos .jpeg y .png</p>
                                 <input type="file" id="logo" name="logo" accept="image/png, image/jpeg" class="input-file" required/>
+
                             </div>
 
                             <div class="item5" ><!--fila 5-->
@@ -107,18 +122,56 @@ get_header();
                 </div>
             </section>
             <div class ="right">
-                <button class="button-just" name="registrar" id="registrar" onclick="
-                <?php
-                // Funcion registra al patrocinante
-                if(preg_match("/^[a-zA-Z ]*$/",$legal)  and !empty($_POST["legal-name"])) {
-                    add_sponsor($_POST["legal-name"], $_POST["tipo-documento"], $_POST["numero-doc"], $_POST["logo"]);
-                }
-                ?>">REGISTRAR PATROCINANTE</button>
+                <button class="button-just" name="registrar" id="registrar" onclick="registro(this.form, this.form.logo.value)">REGISTRAR PATROCINANTE</button>
             </div>
         </form><!--fin de formulario-->
     </div><!-- fin  area-3 del grid-1 -->
 </section> <!-- fin  grid-1-->
 
+<script language="JavaScript">
+
+    function registro(formulario, archivo) {
+        extensiones_permitidas = new Array(".png", ".jpeg");
+        mierror = "";
+        if (archivo) { // Si existe el archivo
+            //recupero la extensión de este nombre de archivo
+            extension = (archivo.substring(archivo.lastIndexOf("."))).toLowerCase();
+            //compruebo si la extensión está entre las permitidas
+            permitida = false;
+            document.cookie = "valid= false";
+            for (var i = 0; i < extensiones_permitidas.length; i++) {
+                if (extensiones_permitidas[i] == extension) {
+                    permitida = true;
+                    document.cookie = "valid= true";
+                    break;
+                }
+            }
+            if (!permitida) {
+                mierror = "Comprueba la extensión de los archivos a subir. \nSólo se pueden subir archivos con extensiones: " + extensiones_permitidas.join();
+            }else
+                if(permitida) {
+                    //submito!
+                    <?php
+                    // Funcion registra al patrocinante
+                    $permitido = $_COOKIE["valid"];
+                    if (preg_match("/^[a-zA-Z ]*$/", $legal) and !empty($_POST["legal-name"]) and $permitido=="true" and sizeof(search_sponsor_id($_POST["numero-doc"],$_POST["tipo-documento"])) == 0) {
+                            add_sponsor($_POST["legal-name"], $_POST["tipo-documento"], $_POST["numero-doc"], $_POST["logo"], $_POST["aporte"]);
+                        echo "<script languaje='javascript'>alert('Material en depósito : ')</script>";
+                    }
+                    ?>
+                    //alert("Usuario Registrado correctamente");
+
+                    return 1;
+                }
+
+            //si estoy aqui es que no se ha podido submitir
+            alert (mierror);
+        }
+
+        return 0;
+    }
+
+</script>
 
 </body>
 
