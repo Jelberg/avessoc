d2d5d6bde59406505efc7b8801b76b10e617862e
Jessica Elberg <jessica_elberg@hotmail.com>
2018-09-13 20:57:18 -0400

    Agregado funcionamiento para que la lista del sponsor
    Falta hacer la pruebas, hecho en casa mas no se puede ejecutar

--- php/template/template-search-sponsor-index.php
+++ php/template/template-search-sponsor-index.php
@@ -32,5 +32,32 @@
 
 </script>
 
+<?php
+
+function llenaListaPatrocinantes(){
+    global $wpdb;
+
+    $query ="SELECT MPERSON_LEGAL_NAME, MPERSON_TYPE_DOC, MPERSON_IDENTF FROM SPONSOR;";
+    $count ="SELECT COUNT(MPERSON_LEGAL_NAME) FROM SPONSOR;";
+    $consulta= $wpdb->get_results($query);
+    $total= $wpdb->get_var($query);
+    $lista="";
+
+    if ($total > 0 ){
+        while($consulta = $row->fetch_array()){
+        $lista .= "<tr>\n";
+        $lista .= '<td>'.$row[1]."</td>\n";
+        $lista .= '<td>'.$row[2]."</td>\n";
+        $lista .= '<td>'.$row[3]."</td>\n";
+        $lista .= "<tr>\n";
+        }
+
+    }
+    return $lista;
+}
+
+
+?>
+
 </body>
 </html>
--- php/template/template-search-sponsor.php
+++ php/template/template-search-sponsor.php
@@ -54,64 +54,24 @@ include "template-search-sponsor-index.php";
                         <table id="example" class="display" style="width:70%">
                             <thead>
                             <tr>
-                                <th>Name</th>
-                                <th>Position</th>
-                                <th>Office</th>
-                                <th>Age</th>
-                                <th>Start date</th>
-                                <th>Salary</th>
+                                <th>Nombre Legal</th>
+                                <th>Tipo de Documento</th>
+                                <th>Número de documento</th>
                             </tr>
                             </thead>
-                            <tbody>
+                            <tbody>    
                             <tr>
                                 <td>Tiger Nixon</td>
                                 <td>System Architect</td>
                                 <td>Edinburgh</td>
-                                <td>61</td>
-                                <td>2011/04/25</td>
-                                <td>$320,800</td>
                             </tr>
-                            <tr>
-                                <td>Tiger Nixon</td>
-                                <td>System Architect</td>
-                                <td>Edinburgh</td>
-                                <td>61</td>
-                                <td>2011/04/25</td>
-                                <td>$320,800</td>
-                            </tr>
-                            <tr>
-                                <td>Garrett Winters</td>
-                                <td>Accountant</td>
-                                <td>Tokyo</td>
-                                <td>63</td>
-                                <td>2011/07/25</td>
-                                <td>$170,750</td>
-                            </tr>
-                            <tr>
-                                <td>Ashton Cox</td>
-                                <td>Junior Technical Author</td>
-                                <td>San Francisco</td>
-                                <td>66</td>
-                                <td>2009/01/12</td>
-                                <td>$86,000</td>
-                            </tr>
-                            <tr>
-                                <td>Cedric Kelly</td>
-                                <td>Senior Javascript Developer</td>
-                                <td>Edinburgh</td>
-                                <td>22</td>
-                                <td>2012/03/29</td>
-                                <td>$433,060</td>
-                            </tr>
+                            <?php llenaListaPatrocinantes(); ?>
                             </tbody>
                             <tfoot>
                             <tr>
-                                <th>Name</th>
-                                <th>Position</th>
-                                <th>Office</th>
-                                <th>Age</th>
-                                <th>Start date</th>
-                                <th>Salary</th>
+                                <th>Nombre Legal</th>
+                                <th>Tipo de Documento</th>
+                                <th>Número de documento</th>
                             </tr>
                             </tfoot>
                         </table>
