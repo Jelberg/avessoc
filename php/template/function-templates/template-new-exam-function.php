f2ce8da7c88ac85334f86a07b24344518e7388a7
Jessica Elberg <jessica_elberg@hotmail.com>
2018-10-09 12:34:13 -0400

    registra nuevos examenes con patologias

--- php/template/function-templates/template-new-exam-function.php
+++ php/template/function-templates/template-new-exam-function.php
@@ -1,3 +1,4 @@
+<?php agregaNuevoExamen(); //Registra un nuevo examen ?>
 <html>
 <head>
     <script language="JavaScript">
@@ -12,6 +13,19 @@
             } else{
                 document.getElementById("otra-patologia").style.display = "none";
                 document.getElementById("nueva-patologia").required = false;
+            }
+        }
+
+        /**
+         * Muestra el tpe input de otro tipo de categoria
+         */
+        function muestraTextAreaCategoria() {
+            if (document.getElementById("tipo-categoria").value == -1) {
+                document.getElementById("otra-categoria").style.display = "block";
+                document.getElementById("nueva-categoria").required = true;
+            } else{
+                document.getElementById("otra-categoria").style.display = "none";
+                document.getElementById("nueva-categoria").required = false;
             }
         }
 
@@ -31,13 +45,74 @@ function muestraPatologias(){
     $combo = ' <label for="name">Patología por la que se realiza dicho examen</label><span class="required">*</span><br>
                <select id="tipo-patologia" name="tipo-patologia" class="select-area-two" onchange="muestraTextAreaPatologia()" required>' ;
     $combo .= '<option value="" selected> >>Seleccione opción<< </option>';
-    $combo .= '<option value="1" selected> opcion</option>';
-    //AQUI CONSULTA DE LA BASE DE DATOS
+    $query="SELECT DISTINCT `DISIEASE_DESC` FROM DISIEASE";
+    global $wpdb;
+    foreach ($wpdb->get_results($query) as $key => $row){
+        $combo .= '<option value="'.$row->DISIEASE_DESC.'" selected>'.$row->DISIEASE_DESC.'</option>';
+    }
+    $combo .= '<option value="-1" selected>Otro</option>';
+    $combo .= '</select>';
+
+    return $combo;
+}
+
+/**
+ * Funcion llena el combo de categorias
+ */
+function muestraCategorias(){
+    $combo = ' <label for="name">Categoria del Examen</label><span class="required">*</span><br>
+               <select id="tipo-categoria" name="tipo-categoria" class="select-area-two" onchange="muestraTextAreaCategoria()" required>' ;
+    $combo .= '<option value="" selected> >>Seleccione opción<< </option>';
+
+    $query="SELECT DISTINCT `EXAM_CATEGORY` FROM `EXAM`";
+    global $wpdb;
+    foreach ($wpdb->get_results($query) as $key => $row){
+        $combo .= '<option value="'.$row->EXAM_CATEGORY.'" selected>'.$row->EXAM_CATEGORY.'</option>';
+    }
 
     $combo .= '<option value="-1" selected>Otro</option>';
     $combo .= '</select>';
 
     return $combo;
+}
+
+function agregaNuevoExamen(){
+    global $wpdb;
+    if (!empty($_POST['examen'])) {
+        if ($_POST['tipo-categoria'] != '-1' && $_POST['tipo-patologia'] != '-1') {
+            $wpdb->insert('EXAM', array(
+                'EXAM_CATEGORY' => $_POST['tipo-categoria'],
+                'EXAM_DISIEASE' => $_POST['tipo-patologia'],
+                'EXAM_DESC' => ucfirst(strtolower($_POST['examen']))
+            ));
+        } else if ($_POST['tipo-categoria'] == '-1' && $_POST['tipo-patologia'] == '-1') {
+            $wpdb->insert('EXAM', array(
+                'EXAM_CATEGORY' => ucfirst(strtolower($_POST['nueva-categoria'])),
+                'EXAM_DISIEASE' => ucfirst(strtolower($_POST['nueva-patologia'])),
+                'EXAM_DESC' => ucfirst(strtolower($_POST['examen']))
+            ));
+
+            $wpdb->insert('DISIEASE', array(
+                'DISIEASE_DESC' => ucfirst(strtolower($_POST['nueva-patologia']))
+            ));
+        } else if ($_POST['tipo-categoria'] == '-1' && $_POST['tipo-patologia'] != '-1') {
+            $wpdb->insert('EXAM', array(
+                'EXAM_CATEGORY' => ucfirst(strtolower($_POST['nueva-categoria'])),
+                'EXAM_DISIEASE' => $_POST['tipo-patologia'],
+                'EXAM_DESC' => ucfirst(strtolower($_POST['examen']))
+            ));
+        } else if ($_POST['tipo-categoria'] != '-1' && $_POST['tipo-patologia'] == '-1') {
+            $wpdb->insert('EXAM', array(
+                'EXAM_CATEGORY' => $_POST['tipo-categoria'],
+                'EXAM_DISIEASE' => ucfirst(strtolower($_POST['nueva-patologia'])),
+                'EXAM_DESC' => ucfirst(strtolower($_POST['examen']))
+            ));
+
+            $wpdb->insert('DISIEASE', array(
+                'DISIEASE_DESC' => ucfirst(strtolower($_POST['nueva-patologia']))
+            ));
+        }
+    }
 }
 
 ?>
--- php/template/template-new-exam.php
+++ php/template/template-new-exam.php
@@ -24,26 +24,33 @@ include "function-templates/template-new-exam-function.php";
     </div> <!-- fin area 2-->
 
     <div class="area-3">
-        <form id="form-new-exam" name="form-new-exam">
+        <form id="form-new-exam" name="form-new-exam" METHOD="post">
         <section class="grid-2">
             <h3>Agregar Nuevo Examen</h3>
             <div class="item1">
-
-
                     <section class="grid-rows">
                         <div class="item-1">
                             <label for="name">Nombre del examen</label><span class="required">*</span><br>
                             <input type="text" name="examen" id="examen" class="form-area-two">
-                        </div><br>
+                        </div>
                         <div class="item-2">
+                            <?php
+                            echo muestraCategorias();
+                            ?>
+                        </div>
+                        <div class="item-3" style="display: none|block" id="otra-categoria">
+                            <label for="name">En caso de ser otro especifique la Categoría:</label><span class="required">*</span><br>
+                            <input type="text" name="nueva-categoria" id="nueva-categoria" class="form-area-two"><br>
+                        </div>
+                        <div class="item-4">
                             <?php
                             echo muestraPatologias();
                             ?>
-                        </div><br>
-                        <div class="item-3" style="display: none|block" id="otra-patologia">
-                            <label for="name">En caso de ser otro especifique la patologia:</label><span class="required">*</span><br>
+                        </div>
+                        <div class="item-5" style="display: none|block" id="otra-patologia">
+                            <label for="name">En caso de ser otro especifique la Patologia:</label><span class="required">*</span><br>
                             <input type="text" name="nueva-patologia" id="nueva-patologia" class="form-area-two"><br>
-                        </div><br>
+                        </div>
                     </section>
             </div><!--fin del item 1 de grid-2 -->
         </section>
@@ -53,6 +60,11 @@ include "function-templates/template-new-exam-function.php";
         </form>
     </div><!-- fin  area-3 del grid-container -->
 </section> <!-- fin  grid-container-->
-
+<script language="JavaScript">
+    document.getElementById("tipo-categoria").value="";
+    document.getElementById("tipo-patologia").value="";
+    muestraTextAreaPatologia();
+    muestraTextAreaCategoria();
+</script>
 </body>
 
